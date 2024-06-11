a250e27808d82f18dcc15f41cf619337844c3ceb
Samir Talic <4886talga@gmail.com>
2024-06-11 12:59:24 +0200

    add Static Image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Bouquet Grand Prix"
+        src="https://flora.se/wp-content/uploads/2019/06/BukettGrandPrixLyx.png"
+      />
     </Layout>
   );
 };
